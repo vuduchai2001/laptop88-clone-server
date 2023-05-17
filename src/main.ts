8266fcb526ac3c3cab7b0b69a4e9cbecf0106ff9
Vu Duc Hai <hai13032001@gmail.com>
2023-05-17 10:49:44 +0700

    .

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,16 @@ import * as cors from 'cors';
 async function bootstrap () {
   const app = await NestFactory.create( AppModule );
   app.useGlobalPipes( new ValidationPipe() );
-  app.use( cors( {
-    origin: 'http://localhost:3005',
-    allowedHeaders: 'Content-Type, Accept',
-  } ) );
+  app.use( cors( [
+    {
+      origin: 'http://localhost:3005',
+      allowedHeaders: 'Content-Type, Accept',
+    },
+    {
+      origin: 'https://laptop99.netlify.app/',
+      allowedHeaders: 'Content-Type, Accept',
+    }
+  ] ) );
   await app.listen( 3000, () => { } );
 }
 bootstrap();
